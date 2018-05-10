b3fdfcf848c16855f1b8a9bee6df95cb9e6dc1a1
Cameron <cameron.taylor.ninja@gmail.com>
2018-05-10 12:26:20 -0700

    some d20 shit

---
+++ FulpTron/MyCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace FulpTron
+{
+    public class MyCommands
+    {
+        [Command("Hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+        }
+
+        [Command("Roll")]
+        public async Task Roll20(CommandContext ctx, int min = 0, int max = 20)
+        {
+            var rnd = new Random();
+            await ctx.RespondAsync($"🎲 You rolled a: {rnd.Next(min, max)}");
+        }
+
+    }
+}
--- FulpTron/Program.cs
+++ FulpTron/Program.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus.Net.WebSocket;
+using DSharpPlus.CommandsNext;
 
 namespace FulpTron
 {
     class Program
     {
-
         static DiscordClient discord;
+        static CommandsNextModule commands;
 
         static void Main(string[] args)
         {
@@ -23,8 +24,11 @@ namespace FulpTron
             discord = new DiscordClient(new DiscordConfiguration
             {
                 Token = "MzgxNjA0MjgxOTY4NjIzNjE3.DdWLWg.cbPPqKtmjY1Yp-GaDlu3ZVLmLOM",
-                TokenType = TokenType.Bot
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
             });
+
 
             discord.SetWebSocketClient<WebSocket4NetClient>();
 
@@ -32,10 +36,23 @@ namespace FulpTron
             {
                 if (e.Message.Content.ToLower().StartsWith("are we talking about tom fulp"))
                     await e.Message.RespondAsync("I **LOVE** talking about Tom Fulp");
+                if (e.Message.Content.ToLower().StartsWith("good bot"))
+                {
+                    await e.Message.RespondAsync("Yeah I know");
+                }
             };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "fulp"
+            });
+
+            commands.RegisterCommands<MyCommands>();
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
+
+        
     }
 }
