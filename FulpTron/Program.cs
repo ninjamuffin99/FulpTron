9172e9f9fa1445db0779051ee9c1623f1aba61d1
Brandon <brandy@scarecrowarts.com>
2018-05-13 15:22:48 -0400

    oof

--- FulpTron/Program.cs
+++ FulpTron/Program.cs
@@ -3,10 +3,13 @@ using DSharpPlus;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using DSharpPlus.Net.WebSocket;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Interactivity;
+using DSharpPlus.Entities;
 
 namespace FulpTron
 {
@@ -45,19 +48,49 @@ namespace FulpTron
             {
                 if (e.Message.Content.ToLower().StartsWith("are we talking about tom fulp"))
                     await e.Message.RespondAsync("I **LOVE** talking about Tom Fulp");
+
+                if (e.Message.Content.ToLower().StartsWith("hi") ||
+                    e.Message.Content.ToLower().StartsWith("hi") && 
+                    e.Message.Content.ToLower().EndsWith("tom") ||
+                    e.Message.Content.ToLower().EndsWith("fulp") ||
+                    e.Message.Content.ToLower().EndsWith("tom fulp") ||
+                    e.Message.Content.ToLower().EndsWith("fulperino") ||
+                    e.Message.Content.ToLower().EndsWith("fulpster"))
+                {
+                    await e.Message.RespondAsync($"👋 Hi friend!");
+                }
+
                 if (e.Message.Content.ToLower().StartsWith("good bot"))
                 {
-                    await e.Message.RespondAsync("Yeah I know");
+                    await e.Message.RespondAsync("Yeah, I know");
                 }
 
                 if (e.Message.Content.ToLower().StartsWith("shame on you"))
                 {
-                    await e.Message.RespondAsync("**SHAAAAAME!!!**");
+                    if (e.Message.Content.Length > 13)
+                    {
+                        await e.Message.RespondAsync("**SHAAAAAME on " + new string(e.Message.Content.Skip(13).ToArray()) + "!!!**");
+                    }
+                    else
+                    {
+                        await e.Message.RespondAsync("**SHAAAAAME!!!**");
+                    }
                 }
 
                 if (e.Message.Content.ToLower().StartsWith("lies lies lies"))
                 {
-                    await e.Message.RespondAsync("https://www.youtube.com/watch?v=v6cn0mLJVZY");
+                    await e.Message.RespondAsync("yeah!\n\nhttps://www.youtube.com/watch?v=v6cn0mLJVZY");
+                }
+
+                if (e.Message.Content.ToLower().EndsWith("loves lolis") || e.Message.Content.ToLower().StartsWith("i love lolis"))
+                {
+                    await e.Message.RespondAsync("me too!");
+                    await e.Message.RespondWithFileAsync("images/tomloveslolis.jpg");
+                }
+
+                if(e.Message.Content.ToLower().StartsWith("i hate lolis"))
+                {
+                    await e.Message.RespondAsync(";(");
                 }
             };
 
